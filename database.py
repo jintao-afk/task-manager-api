9328d2d11bf2b1d68e9f22cc1de22659fa971c4f
jintao <jintaoliu2002@gmail.com>
2026-09-14 19:17:37 +0800

    feat: support DATABASE_URL configuration

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlmodel import SQLModel, create_engine, Session
 from models import Task
 
-sqlite_url = "sqlite:///tasks.db"
+sqlite_url = os.getenv("DATABASE_URL", "sqlite:///tasks.db")
 engine = create_engine(sqlite_url, echo=False)
 
 def create_db_and_tables():
